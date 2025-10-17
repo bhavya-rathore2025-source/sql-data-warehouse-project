dac688ba4977729c8169af29c826e9d48969a8c0
Bhavya <bhavyaprataprathore.bca2025@spsu.ac.in>
2025-10-18 01:39:55 +0530

    created silver tables and inserting data after data transformation

---
+++ scripts/Silver/inserting.py
@@ -0,0 +1,160 @@
+import pyodbc
+from datetime import datetime
+
+'''
+This Script will insert data into the silver layer tables
+'''
+
+
+# --- Connection details ---
+server = r'DESKTOP-5EU8KIE\SQLEXPRESS'      # e.g. r'DESKTOP-12345\\SQLEXPRESS'
+database = 'DataWarehouse'  # e.g. 'mydb'
+
+
+
+
+# --- Create connection ---
+conn = pyodbc.connect(
+    'DRIVER={SQL Server};'
+    f'SERVER={server};DATABASE={database};')
+
+# --- Create cursor ---
+cursor = conn.cursor()
+
+cursor.execute(r""" 
+BEGIN TRANSACTION;
+TRUNCATE TABLE Silver.crm_cust_info;
+TRUNCATE TABLE Silver.crm_sales_details;
+TRUNCATE TABLE Silver.erp_CUST_AZ12;
+TRUNCATE TABLE Silver.erp_LOC_A101;
+TRUNCATE TABLE Silver.erp_PX_CAT_G1V2;
+INSERT INTO Silver.crm_cust_info (
+    cst_id,
+    cst_key,
+    cst_firstname,
+    cst_lastname,
+    cst_material_status,
+    cst_gndr,
+    cst_create_date
+)
+SELECT 
+    cst_id,
+    cst_key,
+    TRIM(cst_firstname) AS cst_firstname,
+    TRIM(cst_lastname) AS cst_lastname,
+    CASE cst_material_status
+        WHEN 'M' THEN 'Married'
+        WHEN 'S' THEN 'Single'
+    END AS cst_material_status,
+    CASE cst_gndr 
+        WHEN 'M' THEN 'Male'
+        WHEN 'F' THEN 'Female'
+    END AS cst_gndr,
+    cst_create_date
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS Rank1
+    FROM bronze.crm_cust_info
+    WHERE cst_id IS NOT NULL
+) t
+WHERE Rank1 = 1;
+
+
+
+insert into Silver.crm_prd_info(
+	prd_id,
+	cat_id,
+    prd_key ,
+    prd_nm ,
+    prd_cost,
+    prd_line ,
+    prd_start_dt ,
+    prd_end_dt
+)
+select 
+prd_id,
+replace(substring(prd_key,1,5),'-','_')cat_id,
+substring(prd_key,7,len(prd_key))prd_key,
+prd_nm,
+ISNULL(prd_cost,0) prd_cost,
+CASE UPPER(TRIM(prd_line))
+    WHEN 'M' THEN 'Mountain'
+    WHEN 'R' THEN 'Road'
+    WHEN 'S' THEN 'Other Sales'
+    WHEN 'T' THEN 'Touring'
+    ELSE 'n/a'
+END AS prd_line,
+cast(prd_start_dt as date)prd_start_dt,
+cast(dateadd(day,-1,lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)) as date)prd_end_dt
+from bronze.crm_prd_info 
+
+
+insert into Silver.crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+select 
+sls_ord_num,sls_prd_key,sls_cust_id,
+case when sls_order_dt is null then null
+	when len(sls_order_dt)<8 then null
+	else cast(cast(sls_order_dt as varchar) as date) end sls_order_dt,
+cast(cast(sls_ship_dt as varchar) as date) sls_ship_dt,
+cast(cast(sls_due_dt as varchar) as date) sls_due_dt,
+case when sls_sales <0 or sls_sales is null or sls_sales != abs(sls_price)*sls_quantity then abs(sls_price)*sls_quantity else sls_sales end sls_sales,
+sls_quantity,
+CASE 
+    WHEN sls_price IS NULL THEN ABS(sls_sales) / NULLIF(sls_quantity, 0)
+    ELSE sls_price
+END AS sls_price
+from Bronze.crm_sales_details
+
+insert into Silver.erp_CUST_AZ12(
+		CID,BDATE,GEN
+)
+select 
+case when cid like 'NAS%' then SUBSTRING(cid,4,len(cid)) else cid end CID,
+case when Bdate>getdate() then null else bdate end BDATE,
+case when gen is null or gen = '' then 'n/a' 
+	 when gen='M' then 'Male'
+	 when gen='F' then 'Female'
+	 else gen end GEN
+from bronze.erp_CUST_AZ12
+
+
+insert into Silver.erp_LOC_A101(
+cid,cntry
+)
+SELECT
+  REPLACE(cid, '-', '') AS cid,
+  CASE 
+    WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+    WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+    WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+    ELSE TRIM(cntry)
+  END AS cntry
+FROM bronze.erp_loc_a101;
+
+INSERT INTO silver.erp_px_cat_g1v2
+(id, cat, subcat, maintenance)
+SELECT
+  id,
+  cat,
+  subcat,
+  maintenance
+FROM bronze.erp_px_cat_g1v2;
+COMMIT;
+""")
+
+
+conn.commit()
+# --- Close ---
+cursor.close()
+conn.close()
+print('Executed')
